bb5f7412f93784d68193a892c034c7fe46d265e1
Tomas Trnecka <trnecka@gmail.com>
2020-06-01 15:59:20 +0200

    api fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bb2_cyanide_api",
-    version="0.0.3",
+    version="0.0.4",
     author="Tomas Trnecka",
     author_email="trnecka@gmail.com",
     description="Cyanide BB2 API wrapper",
